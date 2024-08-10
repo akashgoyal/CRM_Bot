2119d12515f27df167e14bca498426ebe3433a23
Akash <akashgoyal7@gmail.com>
2024-08-11 02:46:09 +0530

    updates for render

--- llms.py
+++ llms.py
@@ -43,10 +43,10 @@ selected_llm = my_llm #ai71_falcon_180b_init()
 
 
 ##
-# from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-# def hf_baai_bge_small_init():
-#     embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-#     return embed_model
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+def hf_baai_bge_small_init():
+    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    return embed_model
 
-# from llama_index.core import Settings
-# Settings.embed_model = hf_baai_bge_small_init()
+from llama_index.core import Settings
+Settings.embed_model = hf_baai_bge_small_init()
